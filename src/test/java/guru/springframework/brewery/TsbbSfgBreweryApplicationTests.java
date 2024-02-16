0206a89493e638a57d9db328d66fcc64c19cf6aa
Maciej Kempinski <maciej.kempinski@igt.com>
2024-02-16 17:44:10 +0100

    circleci

--- src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -3,7 +3,6 @@ package guru.springframework.brewery;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TsbbSfgBreweryApplicationTests {
 
